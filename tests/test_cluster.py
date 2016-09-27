cdf9eddb2690ad71e63514a994d5ff7d4e6c7dc6
Ryan.K <ryankung@ieee.org>
2016-09-28 00:57:20 +0800

    added test

---
+++ tests/test_cluster.py
@@ -0,0 +1,19 @@
+from celery import Celery
+from celery_redis_cluster_backend import RedisClusterBackend
+
+
+class Config:
+    CELERY_ENABLE_UTC = True
+    CELERY_TIMEZONE = 'Europe/Istanbul'
+    CELERY_REDIS_CLUSTER_SETTINGS = {'startup_nodes': [
+        {"host": "127.0.0.1", "port": "30001"},
+        {"host": "127.0.0.1", "port": "30002"},
+        {"host": "127.0.0.1", "port": "30003"},
+    ]}
+
+
+def test_basic_set():
+    app = Celery()
+    app.config_from_object(Config)
+    rb = RedisClusterBackend(app=app)
+    rb.set('a', 'b1')
